0153d0e0bfa31f57977ea1f0f3ebc9dee78a8594
derpflections <leehongyi91@gmail.com>
2023-01-24 19:25:20 +0800

    eqwe

--- frontend/public/javascript/test.js
+++ frontend/public/javascript/test.js
@@ -11,7 +11,7 @@ function onLogin() {
                 $("#staff-login").empty()
                 $("#login").replaceWith(`<a class = "nav-link active" href="/login" id = "login">Welcome, ${response.data[0][0].first_name} ${response.data[0][0].last_name}.</a>`) //replaces navbar
                 $("#staff-login").append(`<div class = "login-form container" id = "loginResponse"><div class="text-center"><p>Welcome, ${response.data[0][0].first_name} ${response.data[0][0].last_name}.</p><p>What would you like to do today?</p></div></div>`)
-                $("#loginResponse2").append(`<div class = "py-5"><button type="button" class="btn btn-light mx-5">Add Customer.</button><button type="button" class="btn btn-light mx-5">Add Actor.</button></div>`)
+                $("#loginResponse2").append(`<div class = "py-5 text-center" id = "postLoginOptions"><button type="button" class="btn btn-light mx-5">Add Customer.</button><button type="button" class="btn btn-light mx-5">Add Actor.</button><button type="button" class="btn btn-light mx-5">Edit Account Information</button></div>`)
                 $("#loginResponse2").append(`<button type="submit" class = "btn btn-warning" onclick = logout()>Log-out!</button>`)
             })
     }
